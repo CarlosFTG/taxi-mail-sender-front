a775ea142d12f8c5be952ecac7bf3122222e0657
CarlosFTG <cftejedag@gmail.com>
2024-08-08 13:17:38 +0200

    fecth users and show in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+
+import { DataService } from '../app/services/data.service';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatTableModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'taxi-mail-sender';
+
+  displayedColumns: string[] = ['name','adif_email','status'];
+  dataSource: any = [];
+
+  data: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(data => {
+      
+      this.dataSource.push(data.usuarios[0]);
+      console.log(this.dataSource);
+    });
+  }
 }
